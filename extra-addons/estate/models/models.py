8a6db6ad83402f521b32bdbba3117415379ed18b
Manh Pham <manhpham90vn@icloud.com>
2024-07-12 15:28:39 +0700

    update action

--- extra-addons/estate/models/models.py
+++ extra-addons/estate/models/models.py
@@ -2,6 +2,7 @@
 from datetime import timedelta
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class EStateProperty(models.Model):
@@ -31,6 +32,20 @@ class EStateProperty(models.Model):
                 record.garden_area = 0
                 record.garden_orientation = ''
 
+    def on_status_cancel(self):
+        for record in self:
+            if record.state != 'sold':
+                record.state = 'canceled'
+            else:
+                raise UserError('You cannot cancel a sold property')
+
+    def on_status_sold(self):
+        for record in self:
+            if record.state != 'canceled':
+                record.state = 'sold'
+            else:
+                raise UserError('You cannot sell a canceled property')
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
@@ -52,10 +67,11 @@ class EStateProperty(models.Model):
     ])
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold')],
+        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
+         ('canceled', 'Canceled')],
         default='new', required=True)
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
-    buyer_id = fields.Many2one('res.partner', string='Buyer')
+    buyer_id = fields.Many2one('res.partner', string='Buyer', readonly=True)
     seller_id = fields.Many2one('res.users', string='Seller', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', inverse_name='property_id', string='Offers')
@@ -80,13 +96,24 @@ class EStatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Real Estate Property Offer'
 
+    def on_confirm(self):
+        for record in self:
+            record.status = 'accepted'
+            record.property_id.buyer_id = record.partner_id
+            record.property_id.selling_price = record.price
+            record.property_id.state = 'offer_accepted'
+
+    def on_cancel(self):
+        for record in self:
+            record.status = 'refused'
+
     @api.depends('create_date', 'validity')
     def _compute_date_deadline(self):
         for record in self:
             record.date_deadline = record.create_date or fields.Date.today() + timedelta(days=record.validity)
 
     price = fields.Float(required=True)
-    status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')])
+    status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], readonly=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
     property_id = fields.Many2one('estate.property', string='Property')
     validity = fields.Integer(default=7)
