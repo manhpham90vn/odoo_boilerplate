e8a3725a0882fc45e31b4418a3141a88cc7ce36c
Manh Pham <manhpham90vn@icloud.com>
2024-07-12 14:02:10 +0700

    update

--- scripts/remove-docker.sh
+++ scripts/remove-docker.sh
@@ -1,13 +1,12 @@
-ODOO_VERSION="17"
-ODOO_IMAGE="odoo:$ODOO_VERSION"
-POSTGRES_VERSION="16.2"
-POSTGRES_IMAGE="postgres:$POSTGRES_VERSION"
-PGADMIN_IMAGE="dpage/pgadmin4:latest"
-NETWORK="odoo_default"
+#!/bin/bash
 
-docker rm $(docker stop $(docker ps -a -q --filter "name=odoo_${ODOO_VERSION}_*" --format="{{.ID}}"))
-for image in $ODOO_IMAGE $POSTGRES_IMAGE $PGADMIN_IMAGE; do
-    docker rmi $(docker images -q --filter reference=$image)
+FILTER="odoo_*"
+IMAGES=( odoo:17 postgres:16.2 dpage/pgadmin4:latest )
+
+docker rm $(docker stop $(docker ps -a -q --filter "name=$FILTER"))
+for image in ${IMAGES[@]}
+do
+  docker rmi $(docker images -q --filter reference=$image)
 done
-docker volume ls --quiet --filter name="odoo_*" | grep -E "odoo_*" | awk '{ print $1 }' | xargs -r docker volume rm
-docker network rm $NETWORK
+docker volume ls --quiet --filter "name=$FILTER" | awk '{ print $1 }' | xargs -r docker volume rm
+docker network ls --quiet --filter "name=$FILTER" | awk '{  print $1 }' | xargs -r docker network rm
