3580b32cb6849082a903274ef0c8a554b58b484f
Manh Pham <manhpham90vn@icloud.com>
2024-07-12 14:47:12 +0700

    computed fields

--- extra-addons/estate/models/models.py
+++ extra-addons/estate/models/models.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EStateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area or 0 + record.garden_area or 0
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = record.expected_price
+            for offer in record.offer_ids:
+                if offer.price > record.best_price:
+                    record.best_price = offer.price
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        for record in self:
+            if record.garden:
+                record.garden_area = 10
+                record.garden_orientation = 'north'
+            else:
+                record.garden_area = 0
+                record.garden_orientation = ''
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -20,6 +43,7 @@ class EStateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    total_area = fields.Integer(compute='_compute_total_area', store=True)
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
@@ -35,6 +59,8 @@ class EStateProperty(models.Model):
     seller_id = fields.Many2one('res.users', string='Seller', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', inverse_name='property_id', string='Offers')
+    best_price = fields.Float(compute='_compute_best_price')
+
 
 class EStatePropertyType(models.Model):
     _name = 'estate.property.type'
@@ -54,7 +80,14 @@ class EStatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Real Estate Property Offer'
 
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date or fields.Date.today() + timedelta(days=record.validity)
+
     price = fields.Float(required=True)
     status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')])
     partner_id = fields.Many2one('res.partner', string='Partner')
     property_id = fields.Many2one('estate.property', string='Property')
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline')
